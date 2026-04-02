c63c4a3b65b7ba58ed18e5f6f2c89aad66d283cf
vktrchh <viskas6969@gmail.com>
2026-04-02 19:49:56 +0300

    frame added

--- src/main/java/vvojtyuk/hexeditor/Main.java
+++ src/main/java/vvojtyuk/hexeditor/Main.java
@@ -1,7 +1,11 @@
 package vvojtyuk.hexeditor;
 
+import vvojtyuk.hexeditor.ui.MainFrame;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
     }
 }
---
+++ src/main/java/vvojtyuk/hexeditor/ui/MainFrame.java
@@ -0,0 +1,19 @@
+package vvojtyuk.hexeditor.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame(){
+        initFrame();
+    }
+
+    public void initFrame(){
+        setTitle("Hex Editor");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1200, 800);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);
+    }
+}
