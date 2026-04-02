0d835a44177f901e2608afab841ca13822740bfa
vktrchh <viskas6969@gmail.com>
2026-04-02 20:38:55 +0300

    toolBar view added

--- src/main/java/vvojtyuk/hexeditor/ui/MainFrame.java
+++ src/main/java/vvojtyuk/hexeditor/ui/MainFrame.java
@@ -7,12 +7,18 @@ public class MainFrame extends JFrame {
     private final MainMenuBar mainMenuBar = new MainMenuBar();
     private final JToolBar toolBar = new JToolBar();
     private final HexTable hexTable = new HexTable();
+    private final OffsetTable offsetTable = new OffsetTable();
+
+    private final JTextField bytesInRow = new JTextField("16", 4);
+    private final JTextField visibleRows = new JTextField("16", 4);
 
     public MainFrame(){
         initFrame();
         setJMenuBar(mainMenuBar);
         add(new JTable(hexTable));
-
+        add(new JTable(offsetTable));
+        add(toolBar, BorderLayout.NORTH);
+        initToolBar();
     }
 
     public void initFrame(){
@@ -25,7 +31,9 @@ public class MainFrame extends JFrame {
 
     public void initToolBar(){
         toolBar.add(new JLabel("Байт в строке"));
+        toolBar.add(bytesInRow);
         toolBar.add(new JLabel("Строк"));
+        toolBar.add(visibleRows);
     }
 
 }
